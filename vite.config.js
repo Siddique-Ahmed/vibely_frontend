d14442f4819dfa64a486692eb0ead4c8e1a60854
SiddiqueAhmed <153608121+Siddique-Ahmed@users.noreply.github.com>
2026-05-08 13:03:11 +0500

    integrate pwa

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,47 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Vibely",
+        short_name: "Vibely",
+        description: "Vibely Connect with peoples arround world",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/vibely_logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/vibely_logo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/vibely_logo.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        devOptions: {
+          enabled: true,
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
